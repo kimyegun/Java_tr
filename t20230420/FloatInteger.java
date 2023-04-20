5e1e1ca2fb849b028238f2d4566ed931c5b41beb
kimyegun <kimyegun123@gmail.com>
2023-04-20 16:38:16 +0900

    정숫값대입

---
+++ t20230420/FloatInteger.java
@@ -0,0 +1,15 @@
+// int형 변화에 실숫값을 대입
+package t20230420;
+public class FloatInteger {
+
+	public static void main(String[] args) {
+		int a;
+		
+		a = (int) 10.0;
+	//	a =  10.0;         // 오류
+		
+		System.out.println("a =" + a);
+
+	}
+
+}
