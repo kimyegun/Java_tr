bf4af8e3bd14288ffa8298efd5a5d926febfd735
kimyegun <kimyegun123@gmail.com>
2023-04-24 17:08:04 +0900

    배열구하기

---
+++ t20230424/PrintArrayVariable.java
@@ -0,0 +1,13 @@
+package t20230424;
+
+public class PrintArrayVariable {
+
+	public static void main(String[] args) {
+		int[] a= new int[5];
+		System.out.println("a = " + a);
+		a=null;
+		System.out.println("a = " + a);
+
+	}
+
+}
