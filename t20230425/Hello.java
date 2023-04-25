98b842232ae7f8d9c59ed0a3a45cbf029ce4a49b
kimyegun <kimyegun123@gmail.com>
2023-04-25 16:52:35 +0900

    안녕하세요

---
+++ t20230425/Hello.java
@@ -0,0 +1,16 @@
+package t20230425;
+
+public class Hello {
+	//---안녕하세요 라고 표시 ---//
+	static void hello() {
+		System.out.println("안녕하세요.");
+		
+	}
+	public static void main(String[] args) {
+		hello();
+		hello();
+		hello();
+		
+	}
+
+}
