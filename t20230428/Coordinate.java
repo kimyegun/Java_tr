a8112875170ded6c34f667dca7c7b21cf59241c7
kimyegun <kimyegun123@gmail.com>
2023-04-28 18:41:16 +0900

    코디네이트(ver_2)

--- t20230428/Coordinate.java
+++ t20230428/Coordinate.java
@@ -5,15 +5,19 @@ public class Coordinate {
 	private double y; //y좌표
 	
 	//---생성자---//
-	Coordinate(double x, double y) { this.x = x; this.y = y; }
+	public Coordinate() { }
+	public Coordinate(double x, double y) { set(x,y); }
+	public Coordinate(Coordinate c) { this(c.x, c.y);}
 	
-	double getX() { return x;} 		// x좌표 가져오기
-	double getY() { return y;} 		// y좌표 가져오기
+	public double getX() { return x;} 		// x좌표 가져오기
+	public double getY() { return y;} 		// y좌표 가져오기
 
-	void setX(double x) {this.x=x;}	//x좌표 설정
-	void setY(double y) {this.y=y;}	//y좌표 설정
+	public void setX(double x) {this.x=x;}	//x좌표 설정
+	public void setY(double y) {this.y=y;}	//y좌표 설정
 	
-	void set(double x, double y) {this.x = x; this.y = y; }		//좌표설정
+	public void set(double x, double y) {this.x = x; this.y = y; }		//좌표설정
+	public boolean equals(Coordinate c) {return x == c.x && y == c.y;}		//좌표설정
+	public String toString() {return "(" + x + "," + y + ")";}
 }
 
 
