7791840ef2ada571cc621d3b33e526b7f02a9aab
kimyegun <kimyegun123@gmail.com>
2023-05-01 18:25:41 +0900

    사람클래스

---
+++ t20230501/Human3.java
@@ -0,0 +1,5 @@
+package t20230501;
+
+public class Human3 {
+
+}
---
+++ t20230501/HumanTester3.java
@@ -0,0 +1,10 @@
+package t20230501;
+
+public class HumanTester3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
