387e19c9135311b0d86e4d18b489ced800f1d991
kimyegun <kimyegun123@gmail.com>
2023-05-02 17:48:06 +0900

    원의면적

---
+++ t20230502/Mathmethods.java
@@ -0,0 +1,18 @@
+package t20230502;
+
+import java.util.Scanner;
+
+public class Mathmethods {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("실수값:");
+		double x = stdIn.nextDouble();
+		
+		System.out.println("절댓값: " + Math.abs(x));
+		System.out.println("제곱근: " + Math.sqrt(x));
+		System.out.println("면 적 : " + Math.PI * x * x);
+	}
+
+}
