6c8715cd078018c018eabcc2e594d237fba28786
kimyegun <kimyegun123@gmail.com>
2023-05-02 17:44:48 +0900

    정수형의표현범위

---
+++ t20230502/IntegerLimits.java
@@ -0,0 +1,13 @@
+package t20230502;
+
+public class IntegerLimits {
+
+	public static void main(String[] args) {
+		System.out.println("정수형의 표현 범위");
+		System.out.println("byte 형:" + Byte.MIN_VALUE + "~" + Byte.MIN_VALUE);
+		System.out.println("short형:" + Short.MIN_VALUE + "~" + Short.MAX_VALUE);
+		System.out.println("int  형:" + Integer.MIN_VALUE + "~" + Integer.MAX_VALUE);
+		System.out.println("long 형:" + Long.MIN_VALUE + "~" + Long.MAX_VALUE);
+	}
+
+}
